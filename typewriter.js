4bff5cf7d9571fe21d6dfc38ef1b4e6e23ad8157
kurtenj <jonathan.kurten@datastax.com>
2023-07-19 21:51:52 -0500

    Style updates, add disclaimer, sync TinyLetter

--- typewriter.js
+++ typewriter.js
@@ -1,17 +1,15 @@
 var i = 0;
 var txt = 'By neighbors, for neighbors.';
-var speed = 100; // Speed of the effect
+var speed = 90; // Increased speed of the effect
 
 function typeWriter() {
     if (i < txt.length) {
-        var span = document.createElement("span");
-        span.innerText = txt.charAt(i);
-        if (txt.charAt(i) === ".") {
-            span.className = "end-period"; // Add a specific class to the period
-        }
-        document.getElementById("title").appendChild(span);
+        document.getElementById("title").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
+    } else {
+        // Hide the placeholder once the typing is finished
+        document.querySelector(".title-placeholder").style.display = 'none';
     }
 }
 
