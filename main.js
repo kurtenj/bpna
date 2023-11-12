d32da5546f019b10bf435ebace8ec114a345a8cb
kurtenj <jonathan.kurten@datastax.com>
2023-11-12 14:51:47 -0600

    Remove map, style actions

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// Mapbox
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3VydGVuaiIsImEiOiJjaXQ2OTdhaG0wMDNhMnRvMzFlZnNsdWI5In0.GpDWv3s4uwpgbnIEmoVbSw';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v11',
-    center: [-94.21016, 46.5997],
-    zoom: 14.26,
-    interactive: false
-});
-
 // Typewriter effect
 var i = 0;
 var txt = 'By neighbors, for neighbors.';
@@ -24,30 +14,3 @@ function typeWriter() {
 document.addEventListener('DOMContentLoaded', (event) => {
     typeWriter();
 });
-
-// Parallax settings
-const parallaxFactor = 0.0005; // Adjust this to change the parallax effect strength.
-let targetX = 0, targetY = 0;
-let currentX = 0, currentY = 0;
-const smoothFactor = 0.05; // Adjust this to change the smoothness of the parallax effect.
-
-// Handle mouse move
-document.addEventListener('mousemove', (event) => {
-    const x = event.clientX / window.innerWidth - 0.5;
-    const y = event.clientY / window.innerHeight - 0.5;
-
-    targetX = x * parallaxFactor;
-    targetY = y * parallaxFactor;
-});
-
-// Animate map movement
-function animate() {
-    currentX += (targetX - currentX) * smoothFactor;
-    currentY += (targetY - currentY) * smoothFactor;
-
-    map.setCenter([-94.21016 - currentX, 46.5997 - currentY]);
-
-    requestAnimationFrame(animate);
-}
-
-animate();
