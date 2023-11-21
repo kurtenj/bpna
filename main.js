73d55fa1c0a1c4ab82209acffb4afd4e39ff283d
kurtenj <jonathan.kurten@datastax.com>
2023-11-20 21:12:10 -0600

    Fixes and optimization

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 // Typewriter effect
 var i = 0;
-var txt = 'By neighbors, for neighbors.';
-var speed = 90;
+var txt = 'By neighbors,<br>for neighbors.';
+var speed = 80;
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("title").innerHTML += txt.charAt(i);
-    i++;
+    // Check for the line break and insert it accordingly
+    if (txt.substring(i, i + 4) === "<br>") {
+      document.getElementById("title").innerHTML += "<br>";
+      i += 4; // Skip the <br> tag length
+    } else {
+      document.getElementById("title").innerHTML += txt.charAt(i);
+      i++;
+    }
     setTimeout(typeWriter, speed);
   }
 }
